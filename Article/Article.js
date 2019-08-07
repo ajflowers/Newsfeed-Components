3e584fe5571f0e0c0a7fd47106ce0a32372fda8f
Angela Flowers <me@angela.flowers>
2019-08-07 16:06:16 -0400

    completed articles.js

--- Article/Article.js
+++ Article/Article.js
@@ -85,7 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
+  },
+  {
+    title: "BUILD WEEK: Lambda Students Share Their Experiences",
+    date: "August 5, 2019",
+    firstParagraph: "Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium. Qui offenderit rapto, terribilem incessu. The voodoo sacerdos suscitat mortuos comedere carnem. Search for solum oculi eorum defunctis cerebro. Nescio an Undead zombies. Sicut malus movie horror.", 
+    secondParagraph: "Cum horribilem resurgere de sepulcris creaturis, sicut de iride et serpens. Pestilentia, ipsa screams. Pestilentia est haec ambulabat mortuos. Sicut malus voodoo. Aenean a dolor vulnerum aperire accedunt, mortui iam vivam. Qui tardius moveri, sed in magna copia sint terribiles legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi odores aere implent.",
+    thirdParagraph: "Tremor est vivos magna. Expansis ulnis video missing carnem armis caeruleum in locis. A morbo amarus in auras. Nihil horum sagittis tincidunt, gelida portenta. The unleashed virus est, et iam mortui ambulabunt super terram. Souless mortuum oculos attonitos back zombies. An hoc incipere Clairvius Narcisse, an ante? Is bello mundi z?"
+  },
+  
 ];
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
@@ -114,18 +122,14 @@ const data = [
 */
 
 
-// data.forEach(story => {
-//   storyBox.appendChild(editor(story.title, story.date, story.firstParagraph, story.secondParagraph, story.thirdParagraph));
-// })
-
 //prevents null errors from code running before all resources load 
 window.addEventListener("load", function(event){
 
   //all code below runs when load event triggered
 
   //retrieve articles div
-  const articles = document.querySelector(".articles");
-  console.log(articles);
+  const articles = document.querySelectorAll(".articles");
+  //console.log(articles); 
   
   //component-building function
   const editor = function (object) {
@@ -148,7 +152,7 @@ window.addEventListener("load", function(event){
     article.appendChild(articleBtn);
 
     //classes
-
+    article.className = "article";
     articleDate.className = "date";
     articleBtn.className = "expandButton";
 
@@ -158,6 +162,17 @@ window.addEventListener("load", function(event){
     articleP1.textContent = object.firstParagraph;
     articleP2.textContent = object.secondParagraph;
     articleP3.textContent = object.thirdParagraph;
+    articleBtn.textContent = "Read more";
+
+    //event listener
+    articleBtn.addEventListener("click", (e) => {
+      article.classList.toggle("article-open");
+      if (article.classList.contains("article-open")) {
+        articleBtn.textContent = "collapse";
+      } else {
+        articleBtn.textContent = "Read more";
+      }
+    });
 
     //return the article
     return article;
@@ -167,7 +182,7 @@ window.addEventListener("load", function(event){
 
   //append content from data
   data.forEach(story => {
-    articles.appendChild(editor(story));
+    articles[0].prepend(editor(story));
   })
 
   //console.log(editor(data[0]));
